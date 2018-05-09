06f46dc37f5dd2dbd93d74f186daaebd8eaa81bd
JaeyeonKIM <foodsksms@gmail.com>
2018-05-10 00:21:03 +0900

    make qna project based on gradle

---
+++ qna-jimmy/src/main/java/com/example/qnajimmy/QnaJimmyApplication.java
@@ -0,0 +1,12 @@
+package com.example.qnajimmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaJimmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaJimmyApplication.class, args);
+	}
+}
