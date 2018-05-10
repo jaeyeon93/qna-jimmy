c8f5c18b401cc8fdcb0dbd3659eb00446008bed4
JaeyeonKIM <foodsksms@gmail.com>
2018-05-10 21:28:17 +0900

    add gradle file

--- src/main/java/codesqaud/QnaJimmyApplication.java
+++ src/main/java/codesqaud/QnaJimmyApplication.java
@@ -1,11 +1,10 @@
 package codesqaud;
 
 import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QnaJimmyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QnaJimmyApplication.class, args);
 	}
