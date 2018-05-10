1d8ede404e56d3c18e5ae531a966781585db650e
jimmy <foodsksms@gmail.com>
2018-05-10 13:39:40 +0900

    Error solved

---
+++ src/main/java/codesqaud/QnaJimmyApplication.java
@@ -0,0 +1,12 @@
+package codesqaud;
+
+import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaJimmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaJimmyApplication.class, args);
+	}
+}
